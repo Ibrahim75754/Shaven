6cd3c1c200c2607dbb17f3afb3db6615309eb7cc
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-12 12:20:49 +0600

    banner page setup

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
     ];
     let [open, setOpen] = useState(false);
     return (
-        <div className='shadow-md w-full fixed top-0 left-0'>
+        <div className='shadow-md w-full top-0 left-0'>
       <div className='md:flex items-center justify-between bg-indigo-600 py-4 md:px-10 px-7'>
       <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
       text-white'>
