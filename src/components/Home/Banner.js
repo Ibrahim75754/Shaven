88e44a61351fd4853aa905401ccf871d47304ca1
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-12 13:47:36 +0600

    banner

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Keyboard, Mousewheel, Navigation, Pagination } from 'swiper';
+
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "./Banner.css";
+
+import banner1 from '../../img/banner/banner1.jpg';
+import banner2 from '../../img/banner/banner2.jpg';
+import banner3 from '../../img/banner/banner3.jpg';
+
+SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
 
 const Banner = () => {
     return (
-        <div>
-            <h1>banner</h1>
+        <div className=''>
+        
+        <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper height"
+      >
+        <SwiperSlide><img src={banner1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={banner2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={banner3} alt="" /></SwiperSlide>
+    
+      </Swiper>
         </div>
+        
     );
 };
 
